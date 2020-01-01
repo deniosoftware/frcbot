fdc4b0911c256ce2ca87f30b1fd86704aa109c6c
Caleb Denio <cjdenio44@gmail.com>
2020-01-01 16:56:03 -0500

    Fixed #7

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ app.post('/slack/tba', (req, res) => {
                             })
                         }).catch(reason => {
                             res.json({
-                                response_type: "in_channel",
+                                response_type: "ephemeral",
                                 text: reason == "404" ? "I couldn't find that team. :cry:" : "Something went wrong on our end. Please try again in a little bit."
                             })
                         })
