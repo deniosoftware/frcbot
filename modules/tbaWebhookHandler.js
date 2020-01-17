1268bb65aff19edb2fb8a2ef67834102141a635c
Caleb Denio <cjdenio44@gmail.com>
2020-01-16 19:35:56 -0500

    I think it's done ;)

--- modules/tbaWebhookHandler.js
+++ modules/tbaWebhookHandler.js
@@ -13,105 +13,111 @@ const tbaClient = new TBAClient(process.env.tbaApiKey)
 
 module.exports = (req, res) => {
     res.end()
+
+    var message_data = req.body.message_data
+
     switch (req.body.message_type) {
         case "match_score":
+            var blueTeams = message_data.match.alliances.blue.teams.map(item => item.replace(/^frc/, ""))
+            var redTeams = message_data.match.alliances.red.teams.map(item => item.replace(/^frc/, ""))
+
+            var allTeams = blueTeams.concat(redTeams)
+
             var query = datastore.createQuery('subscriptions')
-                .filter('event', req.body.message_data.match.event_key)
+                .filter('event', message_data.match.event_key)
                 .filter('match_score', true)
 
+            // Get all match score subscriptions for this event
             datastore.runQuery(query, function (err, entities) {
-                if (err) {
-                    console.log(err.message)
-                }
-                else {
-                    entities.forEach(item => {
-                        data.getTeamNumber(item.team_id).then(team => {
-                            if (item.type == "all") {
-                                data.getToken(item.team_id).then(token => {
-                                    slack.postMessage(blockMessages.matchJustPlayed({
-                                        name: req.body.message_data.event_name,
-                                        key: req.body.message_data.match.event_key
+                // Loop over subscriptions
+                entities.forEach(item => {
+                    data.getTeamNumber(item.team_id).then(team => {
+                        var additionalTeams;
+
+                        try {
+                            if (!item.additional_teams) {
+                                throw new Error("woot")
+                            }
+                            additionalTeams = JSON.parse(item.additional_teams)
+                        }
+                        catch (e) {
+                            // Not JSON
+                            additionalTeams = null;
+                        }
+
+                        var subscribedTeams = [...(team ? [team.toString()] : []), ...(additionalTeams || [])]
+
+                        if (item.type == "all" || (item.type == "team" && allTeams.some(item => subscribedTeams.includes(item)))) {
+                            data.getToken(item.team_id).then(token => {
+                                slack.postMessage(blockMessages.matchJustPlayed({
+                                    name: message_data.event_name,
+                                    key: message_data.match.event_key
+                                },
+                                    {
+                                        name: compLevelToString(message_data.match.comp_level) + " " + message_data.match.match_number.toString(),
+                                        key: message_data.match.key
                                     },
-                                        {
-                                            name: compLevelToString(req.body.message_data.match.comp_level) + " " + req.body.message_data.match.match_number.toString(),
-                                            key: req.body.message_data.match.key
-                                        },
-                                        req.body.message_data.match.alliances.red,
-                                        req.body.message_data.match.alliances.blue,
-                                        team), item.channel, token)
-                                })
-                            }
-                            else if (item.type == "team") {
-                                // Is their team in this match?
-                                if (team && (req.body.message_data.match.alliances.red.teams.includes("frc" + team.toString()) || req.body.message_data.match.alliances.blue.teams.includes("frc" + team.toString()))) {
-                                    data.getToken(item.team_id).then(token => {
-                                        slack.postMessage(blockMessages.matchJustPlayed({
-                                            name: req.body.message_data.event_name,
-                                            key: req.body.message_data.match.event_key
-                                        },
-                                            {
-                                                name: compLevelToString(req.body.message_data.match.comp_level) + " " + req.body.message_data.match.match_number.toString(),
-                                                key: req.body.message_data.match.key
-                                            },
-                                            req.body.message_data.match.alliances.red,
-                                            req.body.message_data.match.alliances.blue,
-                                            team), item.channel, token)
-                                    })
-                                }
-                            }
-                        })
+                                    message_data.match.alliances.red,
+                                    message_data.match.alliances.blue,
+                                    team, subscribedTeams), item.channel, token)
+                            })
+                        }
                     })
-                }
+                })
             })
             break;
         case "upcoming_match":
-            request(`https://www.thebluealliance.com/api/v3/match/${req.body.message_data.match_key}`, {
+            var allTeams = message_data.team_keys.map(item => item.replace('frc', ''))
+
+            request(`https://www.thebluealliance.com/api/v3/match/${message_data.match_key}`, {
                 headers: {
                     "X-TBA-Auth-Key": process.env.tbaApiKey
                 }
             }, function (err, resp, body) {
-                if (!err && resp.statusCode == 200) {
-                    body = JSON.parse(body)
+                body = JSON.parse(body)
 
-                    var query = datastore.createQuery('subscriptions')
-                        .filter('event', body.event_key)
-                        .filter('upcoming_match', true)
-                    datastore.runQuery(query, function (err, entities) {
-                        entities.forEach(item => {
-                            data.getTeamNumber(item.team_id).then(number => {
-                                if (item.type == "all") {
-                                    data.getToken(item.team_id).then(token => {
-                                        slack.postMessage(blockMessages.upcomingMatch({
-                                            name: req.body.message_data.event_name,
-                                            key: body.event_key
-                                        }, {
-                                            name: compLevelToString(body.comp_level) + " " + body.match_number.toString(),
-                                            key: req.body.message_data.match_key
-                                        }, req.body.message_data.team_keys, number), item.channel, token)
-                                    })
+                var query = datastore.createQuery('subscriptions')
+                    .filter('event', body.event_key)
+                    .filter('upcoming_match', true)
+                datastore.runQuery(query, function (err, entities) {
+                    entities.forEach(item => {
+                        data.getTeamNumber(item.team_id).then(team => {
+                            var additionalTeams;
+
+                            try {
+                                if (!item.additional_teams) {
+                                    throw new Error("woot")
                                 }
-                                else if (item.type == "team" && number && (req.body.message_data.team_keys.includes("frc" + number.toString()))) {
-                                    data.getToken(item.team_id).then(token => {
-                                        slack.postMessage(blockMessages.upcomingMatch({
-                                            name: req.body.message_data.event_name,
-                                            key: body.event_key
-                                        }, {
-                                            name: compLevelToString(body.comp_level) + " " + body.match_number.toString(),
-                                            key: req.body.message_data.match_key
-                                        }, req.body.message_data.team_keys, number), item.channel, token)
-                                    })
-                                }
-                            })
+                                additionalTeams = JSON.parse(item.additional_teams)
+                            }
+                            catch (e) {
+                                // Not JSON
+                                additionalTeams = null;
+                            }
+
+                            var subscribedTeams = [...(team ? [team.toString()] : []), ...(additionalTeams || [])]
+
+                            if (item.type == "all" || (item.type == "team" && allTeams.some(item => subscribedTeams.includes(item)))) {
+                                data.getToken(item.team_id).then(token => {
+                                    slack.postMessage(blockMessages.upcomingMatch({
+                                        name: message_data.event_name,
+                                        key: body.event_key
+                                    }, {
+                                        name: compLevelToString(body.comp_level) + " " + body.match_number.toString(),
+                                        key: message_data.match_key
+                                    }, message_data.team_keys, team, subscribedTeams), item.channel, token)
+                                })
+                            }
                         })
                     })
-                }
+                })
             })
             break;
         case "schedule_updated":
             // Get event info
-            tbaClient.getEvent(req.body.message_data.event_key).then(event => {
+            tbaClient.getEvent(message_data.event_key).then(event => {
                 var query = datastore.createQuery('subscriptions')
-                    .filter('event', req.body.message_data.event_key)
+                    .filter('event', message_data.event_key)
                     .filter('event_schedule', true)
 
                 datastore.runQuery(query, function (err, entities) {
@@ -126,7 +132,7 @@ module.exports = (req, res) => {
                                         type: "section",
                                         text: {
                                             type: "mrkdwn",
-                                            text: `The match schedule for <https://www.thebluealliance.com/event/${req.body.message_data.event_key}|*${event.name} ${event.year.toString()}*> has been updated.`
+                                            text: `The match schedule for <https://www.thebluealliance.com/event/${message_data.event_key}|*${event.name} ${event.year.toString()}*> has been updated.`
                                         },
                                         accessory: {
                                             type: "button",
@@ -135,7 +141,7 @@ module.exports = (req, res) => {
                                                 text: "View"
                                             },
                                             action_id: "schedule",
-                                            value: req.body.message_data.event_key
+                                            value: message_data.event_key
                                         }
                                     },
                                     {
